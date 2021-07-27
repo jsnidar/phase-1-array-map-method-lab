cba694ea6e1d58e38765e59c680cd6e2ec328024
Jeremiah <jsnidar@gmail.com>
2021-07-27 15:12:01 -0500

    commit tree 66d7714c343bd96559f2975d6f6ede80796ff6f6

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 const tutorials = [
   'what does the this keyword mean?',
   'What is the Constructor OO pattern?',
@@ -10,7 +11,33 @@ const tutorials = [
   'what is the difference between event capturing and bubbling?',
   'what is JSONP?'
 ];
+function stringCapper (string) {
+    //this function capitalizes all words in a string and returns the string
+    let stringArr = string.split(' ')
+    let capStr = stringArr.map(element => element[0].toUpperCase() + element.substring(1))
+    
+    return capStr.join(' ')
+    }
+  
+    //return the new array
 
 const titleCased = () => {
-  return tutorials
+  const newTutorials = []  
+  for (let string of tutorials) {
+      console.log('string', stringCapper(string))
+      newTutorials.push(stringCapper(string))
+    }  
+    return newTutorials;
 }
+
+titleCased(tutorials)
+  //iterate through the array
+  
+      //separate each word in each string into a string array
+        //iterate through the array of the string
+        //replace the old value with the first letter of the word capitalized
+        //join the string back together
+  
+
+
+
